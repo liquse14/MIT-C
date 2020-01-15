a041f6951c7c64e1b2e6698bfdaf648ad10eb5ce
psw <57971744+liquse14@users.noreply.github.com>
2020-01-16 08:58:28 +0900

    Add files via upload

---
+++ cwork/Day02/prob3-1-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>|
+
+int main32423(void)
+{
+	int a, b, c;
+	printf("Á¤¼ö 3°³");
+	scanf("%d,%d,%d", &a, &b, &c);
+	printf("%d x %d + %d = %d\n", a, b, c, a * b + c);
+	return 0;
+}
---
+++ cwork/Day02/prob3_1_3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>|
+
+int main03(void)
+{
+	int num;
+	printf("정수 1개 입력:");
+	scanf("%d", &num);
+	printf("%d\n",num * num);
+	return 0;
+}
